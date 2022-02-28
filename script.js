2658e7289db4f382dbbc11b37a460b03db808cc2
EthanVB123 <ethanvanbruchem@gmail.com>
2022-02-28 11:27:11 +0800

    v0.2 - accuracy and styles

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("question").innerHTML = "Hello";
 var x = 0;
 var y = 0;
 var ans = 0;
-var history = [0,0];
+var acc = [0,0];
 
 document.addEventListener("keydown", function (e) {
     if (e.key === "Enter") {  
@@ -20,12 +20,20 @@ function newQuestion() {
 function checkAnswer() {
     if (document.getElementById("answer").value == ans) {
         // correct
-        history[0] += 1;
+        acc[0] += 1;
+        console.log(acc);
     } else {
         // incorrect
-        history[1] += 1;
+        acc[1] += 1;
     }
     newQuestion();
     document.getElementById("answer").value = null;
+    renderAnalytics();
 }
+
+function renderAnalytics() {
+    var percentageAcc = Math.round(10000*acc[0]/(acc[0]+acc[1]))/100
+    document.getElementById("acc").innerHTML = `Accuracy: ${acc[0]} / ${acc[0] + acc[1]} = ${percentageAcc}%`
+}
+
 newQuestion();
